5baa305474dab835124e0ae68017289c07ea2c8e
Best <best@Bests-MacBook-Pro.local>
2017-09-27 21:47:11 +0700

    first commit

---
+++ src/main/java/com/blog/sonia/demospringbootangular/DemoSpringbootAngularApplication.java
@@ -0,0 +1,12 @@
+package com.blog.sonia.demospringbootangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootAngularApplication.class, args);
+	}
+}
